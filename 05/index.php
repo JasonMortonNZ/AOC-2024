660dd595f8bc4d57cfb03d032df1adf1ed1abd83
Jason <JasonMortonNZ@users.noreply.github.com>
2024-12-10 00:01:19 +1300

    Day 5 part 2

--- 05/index.php
+++ 05/index.php
@@ -3,13 +3,82 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 // Read file content
-$data = explode(PHP_EOL, file_get_contents('test.txt'));
+$data = explode("\n\n", file_get_contents('data.txt'));
 
 // Part 1
-$matrix = [];
+$rules = explode(PHP_EOL, $data[0]);
+$updates = explode(PHP_EOL, $data[1]);
 
-foreach ($data as $line) {
-    if(!empty(trim($line))) {
-        $matrix[] = str_split(trim($line), 1);
+$correct = [];
+$incorrect = [];
+
+foreach ($updates as $update) {
+    $updateSplit = explode(',', $update);
+    $incorrectSplit = $updateSplit;
+    $valid = true;
+
+    foreach ($updateSplit as $i => $v) {
+
+        $applicableRules = array_filter($rules, fn ($r) => str_contains($r, "|$v") || str_contains($r, "$v|"));
+
+        foreach ($applicableRules as $applicableRule) {
+            [$x, $y] = explode('|', $applicableRule);
+            // $updates must contain both numbers to be valid
+            if (in_array($x, $updateSplit) && in_array($y, $updateSplit)) {
+               // X must come before Y
+                $indexX = array_search($x, $updateSplit);
+                $indexY = array_search($y, $updateSplit);
+
+                if ($indexX > $indexY) {
+                    $valid = false;
+                }
+            }
+        }
+    }
+
+    if ($valid) {
+        $correct[] = $updateSplit[floor(sizeof($updateSplit) / 2)];
+    } else {
+        $incorrect[] = $updateSplit;
     }
 }
+
+$answerPartOne = array_sum($correct);
+echo "Part 1 answer: {$answerPartOne}\n";
+
+// Part 2
+$correct = [];
+
+foreach ($incorrect as $split) {
+    $updateSplit = $split;
+    $changed = true;
+
+    while ($changed) {
+        $changed = false;
+        foreach ($updateSplit as $i => $v) {
+
+            $applicableRules = array_filter($rules, fn($r) => str_contains($r, "|$v") || str_contains($r, "$v|"));
+
+            foreach ($applicableRules as $applicableRule) {
+                [$x, $y] = explode('|', $applicableRule);
+                // $updates must contain both numbers to be valid
+                if (in_array($x, $updateSplit) && in_array($y, $updateSplit)) {
+                    // X must come before Y
+                    $indexX = array_search($x, $updateSplit);
+                    $indexY = array_search($y, $updateSplit);
+
+                    if ($indexX > $indexY) {
+                        $updateSplit[$indexX] = $y;
+                        $updateSplit[$indexY] = $x;
+                        $changed = true;
+                    }
+                }
+            }
+        }
+    }
+
+    $correct[] = $updateSplit[floor(sizeof($updateSplit) / 2)];
+}
+
+$answerPartTwo = array_sum($correct);
+echo "Part 2 answer: {$answerPartTwo}\n";
